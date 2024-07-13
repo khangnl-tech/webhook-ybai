b5efa4fce7f9fde4917e38efbea43ae67316a45d
khangnl <khangnl@khangnl.tech>
2024-07-13 11:51:56 +0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.json());
+
+app.post('/webhook', (req, res) => {
+    const data = req.body;
+    console.log('Received webhook data:', data);
+    // Xử lý dữ liệu ở đây
+
+    res.status(200).send('Webhook received');
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
